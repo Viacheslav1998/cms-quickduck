18453dc461392358fdc028c8fe77c5f55f9dcca6
Slavik <amazingSevenfix@gmail.com>
2024-09-30 00:38:21 +0500

    feat: style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,4 +1,21 @@
 <script>
 </script>
 <template>
+  <div class="container">
+    <div class="footer">
+      <p><span style="color: #c0bd56;">Подвал </span><br>тут бывает много чего интерессно<br>
+        <span style="color: #ffd982">Разрабатывает - Вячеслав</span>
+      </p>
+    </div>
+  </div>
 </template>
+<style scoped>
+
+.footer {
+  padding: 20px 10px 1px 10px;
+  margin-top: 20px;
+  background-color: rgb(81, 81, 81);
+  color: white!important;
+}
+
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,16 +10,16 @@ export default defineComponent({
 
   <div class="container">
     <h1 class="logo">CMS</h1>
-    <h3 class="desk-logo">Управление - Quickduck - сайтом</h3>
+    <h3 class="desk-logo">Управление - <span style="color: #c0bd56;">Quickduck</span> - сайтом</h3>
   </div>
 
    <div class="container navi">
-      <nav class="flex">
+      <nav class="d-flex justify-content-center flex-wrap">
         <RouterLink to="/" class="custom-href">Главная</RouterLink>
-        <RouterLink to="/news" class="custom-href">Добавить новость</RouterLink>
-        <RouterLink to="/gardens" class="custom-href">Управления новостями</RouterLink>
-        <RouterLink to="/tech" class="custom-href">Добавить пользователя</RouterLink>
-        <RouterLink to="/about" class="custom-href">Управление пользователями</RouterLink>
+        <RouterLink to="/add" class="custom-href">Добавить новость</RouterLink>
+        <RouterLink to="/news" class="custom-href">Управления новостями</RouterLink>
+        <RouterLink to="/add-user" class="custom-href">Добавить пользователя</RouterLink>
+        <RouterLink to="/users" class="custom-href">Управление пользователями</RouterLink>
       </nav>
    </div>
 </template>
