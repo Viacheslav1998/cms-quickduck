cf6b334139806ee0f0789780caf6cf0c0c54b45b
Slavik <amazingSevenfix@gmail.com>
2024-09-30 00:13:31 +0500

    feat: style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,18 +8,51 @@ export default defineComponent({
 
 <template>
 
-   <div class="container">
-    <h1>CMS</h1>
-    <p>quickduck - панелька управление сайтом</p>
-   </div>
+  <div class="container">
+    <h1 class="logo">CMS</h1>
+    <h3 class="desk-logo">Управление - Quickduck - сайтом</h3>
+  </div>
 
-   <div class="navi">
-      <nav class="flex justify-evenly font-sans text-2xl font-semibold text-slate-200">
-        <RouterLink to="/" class="cursor-pointer hover:text-yellow-300">home</RouterLink>
-        <RouterLink to="/news" class="cursor-pointer hover:text-yellow-300">news</RouterLink>
-        <RouterLink to="/gardens" class="cursor-pointer hover:text-yellow-300">gardens</RouterLink>
-        <RouterLink to="/tech" class="cursor-pointer hover:text-yellow-300">tech</RouterLink>
-        <RouterLink to="/about" class="cursor-pointer hover:text-yellow-300">about us</RouterLink>
+   <div class="container navi">
+      <nav class="flex">
+        <RouterLink to="/" class="custom-href">Главная</RouterLink>
+        <RouterLink to="/news" class="custom-href">Добавить новость</RouterLink>
+        <RouterLink to="/gardens" class="custom-href">Управления новостями</RouterLink>
+        <RouterLink to="/tech" class="custom-href">Добавить пользователя</RouterLink>
+        <RouterLink to="/about" class="custom-href">Управление пользователями</RouterLink>
       </nav>
    </div>
 </template>
+
+<style scoped>
+  .logo{
+    margin: 10px auto 10px auto;
+    padding: 10px;
+    background-color: #a53131;
+    color: white;
+    font-weight: bold;
+  }
+  .desk-logo {
+    margin: 10px auto 10px auto;
+    padding: 10px;
+    background-color: rgb(96, 96, 96);
+    color: white;
+    font-weight: 100;
+  }
+  .navi {
+    margin-top: 20px;
+  }
+  .custom-href {
+    padding: 10px;
+    margin: 5px;;
+    color: #ffffff;
+    text-decoration: none;
+    border-radius: 10px;
+    background-color: rgb(60, 60, 60);
+  }
+  .custom-href:hover {
+    border-radius: 10px;
+    background-color: rgb(86, 85, 85);
+  }
+
+</style>
