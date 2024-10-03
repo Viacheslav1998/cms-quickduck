8c83d044fbf83642be720bc87595a990103c1a4f
Slavik <amazingSevenfix@gmail.com>
2024-10-03 20:10:00 +0500

    quickfix

---
+++ src/pages/AddNews.vue
@@ -0,0 +1,22 @@
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AddNews',
+})
+</script>
+
+<template>
+  <div class="container">
+    <div class="name-page">
+      <h2>Добавить Новость</h2>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.tools {
+  margin-top: 15px;
+}
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
 // src/router.js
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './pages/Home.vue'; 
-import Add from './pages/Add.vue';
+import AddNews from './pages/AddNews.vue';
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/add', component: Add },
+  { path: '/add', component: AddNews },
 ];
 
 const router = createRouter({
