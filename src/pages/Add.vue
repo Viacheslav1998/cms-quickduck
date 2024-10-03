139554d2d2e4c607215ab83e88b48aaf7ad81d09
Slavik <amazingSevenfix@gmail.com>
2024-10-03 06:01:48 +0500

    feat: editor

--- src/pages/Add.vue
+++ src/pages/Add.vue
@@ -1,10 +1,29 @@
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import 'trix';
+import 'trix/dist/trix.css';
 
 export default defineComponent({
   name: 'Add',
-})
+  setup() {
+    const content = ref('');
+    const title = ref(''); 
+    const description = ref(''); 
 
+    const submit = () => {
+      console.log('Название:', title.value);
+      console.log('Описание:', description.value);
+      console.log('Содержимое:', content.value); 
+    };
+
+    return {
+      content,
+      title,
+      description,
+      submit,
+    };
+  },
+});
 </script>
 
 <template>
@@ -13,13 +32,11 @@ export default defineComponent({
       <h2>Добавить новость</h2>
     </div>
     <div class="space-form">
-
-      <form>
+      <form @submit.prevent="submit">
         <div class="form-row">
-
           <div class="col-md-8 mb-3">
-            <label for="namePost">Название поста</label>
-            <input type="text" class="form-control" id="namePost" placeholder="введите название новости*" required>
+            <label for="namePost">Название Новости</label>
+            <input type="text" class="form-control" id="namePost" placeholder="введите название новости*" v-model="title" required>
             <div class="valid-feedback">
               Хорошо!
             </div>
@@ -28,9 +45,24 @@ export default defineComponent({
             </div>
           </div>
 
+          <div class="col-md-8 mb-3">
+            <label for="nameDesk">Описание Новости</label>
+            <input type="text" class="form-control" id="nameDesk" placeholder="введите описание новости*" v-model="description" required>
+            <div class="valid-feedback">
+              Хорошо!
+            </div>
+            <div class="invalid-feedback">
+              Это поле обязательно!
+            </div>
+          </div>
+          <div class="cstm">
+            <label for="editorContent"><span style="color: silver; font-weight: bold;">Контент</span></label>
+            <trix-editor input="editorContent"></trix-editor>
+            <input id="editorContent" type="hidden" v-model="content" />
+          </div>
         </div>
+        <button type="submit" class="btn btn-danger">Отправить</button>
       </form>
-
     </div>
   </div>
 </template>
@@ -48,4 +80,11 @@ export default defineComponent({
   background-color: rgb(50, 50, 50);
 }
 
+.cstm {
+  border-radius: 5px;
+  color: rgb(123, 118, 118)!important;
+  margin: 20px 0;
+  padding: 20px!important;
+  background-color: rgb(255, 255, 255);
+}
 </style>
