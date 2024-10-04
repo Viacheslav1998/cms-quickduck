54d5b4b6a84670ee98db01832879eb5e91e0a5d9
Slavik <amazingSevenfix@gmail.com>
2024-10-04 23:55:50 +0500

    quickfix

--- src/pages/AddNews.vue
+++ src/pages/AddNews.vue
@@ -13,7 +13,16 @@ export default defineComponent({
     </div>
     <div class="custom-space">
       <form>
-      
+        <div class="form-group">
+          <label for="nameNews">Названия Новости</label>
+          <input type="name" class="form-control" id="nameNews" aria-describedby="nameNews">
+          <small id="nameHelp" class="form-text text-muted">то что привлечет внимание</small>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Описание Новости</label>
+          <input type="password" class="form-control" id="exampleInputPassword1">
+          <small id="nameHelp" class="form-text text-muted">Описание должно быть не большим - достаточно 255 символов - коротко о главном</small>
+        </div>
         
         <button type="submit" class="btn btn-primary">Создать</button>
       </form>
