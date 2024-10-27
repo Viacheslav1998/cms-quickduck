c689118e3a089b791008a9130b97bad8bd14030f
Slavik <amazingSevenfix@gmail.com>
2024-10-27 20:09:15 +0500

    feat: test transition

--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -1,28 +1,34 @@
-<template>
-  <div id="app">
-    <h1>Пример уведомления</h1>
-    <button @click="showAlert">Показать уведомление</button>
-  </div>
-</template>
-
 <script>
-import Swal from 'sweetalert2';
-
 export default {
-  name: 'App',
-  methods: {
-    showAlert() {
-      Swal.fire({
-        title: 'Уведомление!',
-        text: 'Это простое уведомление.',
-        icon: 'info',
-        confirmButtonText: 'Закрыть',
-      });
-    },
-  },
-};
+  data() {
+    return {
+      show: true,
+    }
+  }
+}
 </script>
 
+<template>
+
+<button @click="show = !show">
+  отобразить данные или скрыть их
+</button>
+<transition name="slide-fade">
+  <p v-if="show" style="color: white;">Привет</p>
+</transition>
+
+</template>
+
 <style>
-/* Ваши стили */
+.slide-fade-enter-active {
+  transition: all 1s ease-out;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
 </style>
