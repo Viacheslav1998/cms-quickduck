12844cbb0adcdb335775d095da33f829c551ecaf
Slavik <amazingSevenfix@gmail.com>
2024-11-12 05:27:25 +0500

    feat: custom css

--- src/components/EditPopup.vue
+++ src/components/EditPopup.vue
@@ -13,8 +13,9 @@
           <label>Путь к изображению: </label>
           <input type="file" name="path_to_image" class="form-control" id="path_to_image" aria-describedby="path_to_image" @change="onFileChange">
 
-          <button type="submit">Сохранить</button>
-          <button type="button" @click="close">Закрыть</button>
+          <br>
+          <button class="custom-b green-b" type="submit">Cохрани́ть</button>
+          <button class="custom-b red-b" type="button" @click="close">Закры́ть</button>
         </form>
       </div>
     </div>
@@ -68,20 +69,60 @@ export default defineComponent({
 .popup-overlay {
   position: fixed;
   top: 0; left: 0; right: 0; bottom: 0;
-  background: rgba(0, 0, 0, 0.5);
+  background: rgba(84, 81, 92, 0.5);
   display: flex;
   justify-content: center;
   align-items: center;
 }
 
 .popup-content {
-  background: white;
+  background-color :#EB5C52;
   padding: 20px;
   border-radius: 8px;
   width: 300px;
 }
+
+form input {
+  background-color: whitesmoke;
+  border: 2px solid white;
+  border-radius: 6px;
+  padding: 4px;
+  margin-bottom: 10px;
+}
+
+.custom-b {
+  border: none;
+  margin: 4px;
+  transition: all 0.2s ease;
+}
+.green-b {
+  background-color: rgb(66, 103, 5);
+  padding: 10px;
+  border-radius: 10px;
+  font-size: 18px;
+  font-weight: bold;
+  color: white;
+}
+.green-b:hover {
+  background-color: greenyellow;
+  color: black;
+}
+
+.red-b {
+  background-color: rgb(183, 50, 20);
+  padding: 10px;
+  border-radius: 10px;
+  font-size: 18px;
+  font-weight: bold;
+  color: white;
+}
+.red-b:hover {
+  background-color: gold;
+  color: black;
+}
+
 .fade-enter-active, .fade-leave-active {
-  transition: opacity 0.5s;
+  transition: opacity 1s;
 }
 .fade-enter, .fade-leave-to {
   opacity: 0;
