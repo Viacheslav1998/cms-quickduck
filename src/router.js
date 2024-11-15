95005601982a9a7c05fa7c3ba43b281b088fcbe7
Slavik <amazingSevenfix@gmail.com>
2024-11-16 03:00:09 +0500

    quickfix

--- src/router.js
+++ src/router.js
@@ -4,14 +4,12 @@ import Home from './pages/Home.vue';
 import AddNews from './pages/AddNews.vue';
 import News from './pages/News.vue';
 import Test from './testPages/testAttention.vue';
-import TestStatic from './testPages/testStaticData.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/add', component: AddNews },
   { path: '/news', component: News },
   { path: '/test', component: Test },
-  { path: '/test-static', component: TestStatic },
 ];
 
 const router = createRouter({
