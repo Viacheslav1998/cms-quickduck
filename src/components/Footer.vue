b212a09dff4f3d34df9383cd698cdcc7022f5084
Slavik <amazingSevenfix@gmail.com>
2024-11-27 21:10:19 +0500

    quickfix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,8 +3,8 @@
 <template>
   <div class="container">
     <div class="footer">
-      <p><span>Подвал </span>
-        <p style="margin: 10px 5px;">совершенствовать свое приложение без конца - это не предел</p>
+      <p><span>Подвал </span><br>
+        <span style="margin: 10px 5px;">совершенствовать свое приложение без конца - это не предел</span><br><br>
         
         <span style="color:#000; background-color: #E6BF0B; padding: 5px; margin: 5px; border-radius: 5px; margin-top: 10px;"><b>только для личного использования | не для расспространения</b></span>
         <span style="color:#E6BF0B; background-color: #212121; padding: 5px; border-radius: 5px; margin-top: 10px;"><b>Development SilentStorm</b></span>
