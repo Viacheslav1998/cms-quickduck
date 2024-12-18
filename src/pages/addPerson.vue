d2cf70297aa3d277e28b625e2b6b071002f5fc97
Slavik <amazingSevenfix@gmail.com>
2024-12-18 20:30:07 +0500

    feat: formPerson

--- src/pages/addPerson.vue
+++ src/pages/addPerson.vue
@@ -1,24 +1,55 @@
 <script>
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'addPerson',
-
 });
+
 </script>
 
 <template>
   <div class="container">
     <div class="name-page">
-      <h2>Добавление пользователя</h2>
+      <h2>Добавление пользователя - (ручная раздача привелегий)</h2>
     </div>
-    
-    <p>просто потому что тут была ошибка ты решил ничего не показывать да ?</p>
+
+    <div class="custom-space">
+      <form @submit.prevent = "submitPerson">
+
+        <div class="form-group">
+          <label for="name">Имя пользователя</label>
+          <input v-model="name" type="name" class="form-control" id="name" aria-describedby="name" required>
+          <small id="name" class="form-text text-muted">хм...</small>
+        </div> 
+
+        <div class="form-group">
+          <label for="path_to_image">Картинка пользователя</label>
+          <input type="file" name="path_to_image" class="form-control" id="path_to_image" aria-describedby="path_to_image" @change="onFileChange">
+          <small id="path_to_image" class="form-text text-muted">Картинка пользователя</small>
+        </div>
+
+        <div class="space"></div>
+        <button type="submit" class="btn btn-danger">Добавить пользователя</button>
+      </form>
+    </div>
+
+    <div class="custom-space">
+      <p style="color: white; margin-top: 10px;">Это нужно для того что бы просто отработать пользователя - ему не нужно самому регистрироватся он должен просто получить доступ к определенным вещам. Управление привелегиями пользователя через данную страницу</p>
+    </div>
 
   </div>
 </template>
 
 <style scoped>
+.custom-space {
+  margin: 50px auto;
+  padding: 20px;
+  border: 1px solid grey;
+}
+.space {
+  width: 100%;
+  height: 20px;
+}
 .name-page {
   margin-top: 20px;
   padding: 20px;
